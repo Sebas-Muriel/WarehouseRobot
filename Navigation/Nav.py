5f6a65e19459125a6516dbc8a6f9c20adaffbf04
ajen8005 <ajj17b@my.fsu.edu>
2021-02-17 01:49:37 -0500

    Navigation now returns the closest endpoint

--- Navigation/Nav.py
+++ Navigation/Nav.py
@@ -1,17 +1,17 @@
 import pymongo
 import sys
 import re
+import math
 
-
-
-start = "A0"
+START = "A0"
 
 def main(package):
     myclient = MongoDBconnection()
     mydb = myclient["WarehouseMap"]
     mycol_packages = mydb["QRs"]
     mycol_nav = mydb["Navigation"]
-    CreatePath(mycol_nav, mycol_packages, package)
+    endingPoint = CreatePath(mycol_nav, mycol_packages, package)
+    #While loop reading the Intersections/QRs until it gets to the package. 
     return
 
 #Connects to the mongoDB client and prints out the available databases and collections
@@ -26,14 +26,13 @@ def MongoDBconnection():
     print(mydb.list_collection_names())
     return myclient
 
+#Returns an array of the path that the robot needs to travel
 def CreatePath(Nav, QRs, item):
     itemLoc = QRs.find_one({}, {"_id": 0, "Name" :item, "Left": 1, "Right": 1})
     print(itemLoc)
-    convertToGrid(start)
-    #chooseInitialPath(itemLoc.Left, itemLoc.Right)
-    # splitStart = splitNode(start)
-    # print(splitStart)
-    return
+    itemLoc["Left"]  = convertToGrid(itemLoc["Left"])
+    itemLoc["Right"] = convertToGrid(itemLoc["Right"])
+    return chooseInitialPath(itemLoc["Left"], itemLoc["Right"])
 
 #Breaks up the node into the letters and numbers and returns a list
 def splitNode(node):
@@ -44,18 +43,31 @@ def splitNode(node):
     return items
 
 #Converts nodes such as A0 t0 (0,0) their number equivalent 
+#in the number format.
 def convertToGrid(node):
     split = splitNode(node)
     temp = list(split[0])
     split[0] = ord(temp[0]) - 65
     split[1] = int(split[1])
-    print(split)
+    return split
 
+#Calculates the distance between the two starting points
+#Picks the closest one to the start point
 def chooseInitialPath(left, right):
-    leftSplit = splitNode(left)
-    rightSplit = splitNode(right)
-    startSplit = splitNode(start)
-    
+    startSplit = convertToGrid(START)
+    leftval = math.sqrt(((left[0]-startSplit[0])**2 + (left[1]-startSplit[1])**2))
+    rightval = math.sqrt(((right[0]-startSplit[0])**2 + (right[1]-startSplit[1])**2))
+    if (leftval >= rightval):
+        return right
+    else:
+        return left
+
+#Todo Create IR sensor reading code
+def readIRsensors():
+    return
+
+#Todo Read QR code code
+def ReadQR():
     return
 
 if __name__ == "__main__":
