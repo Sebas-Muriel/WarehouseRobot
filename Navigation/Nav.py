376c2e4a763f25f21f60069df5f09bb3d37f1f1f
Ajen8005 <45045045+Ajen8005@users.noreply.github.com>
2021-03-05 20:03:12 -0500

    Update Nav.py

--- Navigation/Nav.py
+++ Navigation/Nav.py
@@ -7,6 +7,8 @@ import re
 import math
 import time
 import serial
+import cv2
+
 
 ser = serial.Serial('/dev/ttyACM0',9600)
 START = "C0"
@@ -137,9 +139,34 @@ def readIRsensors():
 
 #Todo Read QR code code
 def readQR():
-    #activate camera
-    #take in QR code
-    #return data
+    # set up camera object
+    cap = cv2.VideoCapture(0)
+
+    # QR code detection object
+    detector = cv2.QRCodeDetector()
+
+    while True:
+        # get the image
+        _, img = cap.read()
+        # get bounding box coords and data
+        data, bbox, _ = detector.detectAndDecode(img)
+
+        # if there is a bounding box, draw one, along with the data
+        if(bbox is not None):
+            for i in range(len(bbox)):
+                cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i+1) % len(bbox)][0]), color=(255,
+                         0, 255), thickness=2)
+            cv2.putText(img, data, (int(bbox[0][0][0]), int(bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5, (0, 255, 0), 2)
+            if data:
+                print("Package: ", data)
+        # display the image preview
+        cv2.imshow("Package detection", img)
+        if(cv2.waitKey(1) == ord("q")):
+            break
+    # free camera object and exit
+    cap.release()
+    cv2.destroyAllWindows()
     return
 
 #Todo Create code to move robot when searching for a box inbetween nodes
