5cfe4b5efae8419de2edda05dff629b2e5675c71
ajen8005 <ajj17b@my.fsu.edu>
2021-03-12 21:25:01 -0500

    added Motor stuff

--- IR Sensor/master_receive_byte/master_receive_byte.ino
+++ IR Sensor/master_receive_byte/master_receive_byte.ino
@@ -7,33 +7,54 @@
 #define SLAVE_ADDRESS1 0x08
 #define SLAVE_ADDRESS2 0x09
 
-#define Left 0x05
+#define Left 0x01
 #define Right 0x02
 #define Up 0x03
 #define Down 0x04
 #define Stop 0x00
-#define nodeMode 10
-#define tickMode 11
-#define modeChange 12
+
+#define nodeMode 0
+#define tickMode 1
 
 uint8_t address1Val = 0;
 uint8_t address2Val = 0;
 uint8_t intersectionFLG = 0;
-uint8_t mode = 0;
-uint8_t modeFlag = 0;
 time_t current, prev;
 
 char dir = '0';
+char mode = 'A';
+char reset = '0';
+uint8_t data;
 
+
+void convertSerialRead(char);
 void setup()
 {
     Serial.begin(9600);
     Wire.begin();                               // Join I2C bus.
-    pinMode(13, OUTPUT);
+    //pinMode(13, OUTPUT);
 }
 
 void loop()
 {
+    while(mode == 'A')
+    {
+      if (Serial.available() >= 1)
+      {
+        dir = data&0b00001110;
+        mode = data&0b00000001;
+        reset = data&0b10000000;
+      }
+      if (mode == 0b0 || mode == 0b1)
+      {
+        Serial.println("20");
+      }
+
+    }
+    
+
+
+  
     Wire.requestFrom(SLAVE_ADDRESS1, 1);     
 
     // Receive the 'int' from I2C and print it:-
@@ -49,14 +70,6 @@ void loop()
         //Serial.println(address2Val,BIN);                 
     }
 
-    if (mode == 0)
-    {
-      dir = Serial.read();
-      mode = Serial.read();
-      Serial.println(dir);
-      Serial.println(mode);
-    }
-
     if (mode == nodeMode)
     {
       //If there is an intersection send a 1 to the py if not send a 0
@@ -68,38 +81,31 @@ void loop()
       }
       else{
         Serial.println("0");
-        dir = '0';
+        //dir = '0';
       }
   
       if (intersectionFLG == 1)
       {
-        if (Serial.available() == 1)
-        {
-          dir = Serial.read();
-          Serial.println(dir);
+        if (Serial.available() >= 1)
+        { 
+          data = Serial.read();
+          dir = data&0b00001110;
+          mode = data&0b00000001;
+          reset = data&0b10000000;
+//          Serial.print("Data is: ");
+//          Serial.print(data);
+//          Serial.println();
         }
-        else if (Serial.available == 2)
-        {
-          dir = Serial.read();
-          mode = Serial.read();
-        }
-      }
-  
-      if (dir == 35){
-        digitalWrite(13, LOW);
-      }
-      else
-      {
-        digitalWrite(13, HIGH);
       }
   
       if (address2Val != 0xFF){
         intersectionFLG = 0;
       }
+      //Serial.println("IN NODE MODE");
     }
     else if (mode == tickMode)
     {
-      if (address2Val == b"00011111" || adress2Val == b"00001111")
+      if (address2Val == 0b11110000 || address2Val == 0b11111000)
       {
         Serial.println("1");
       }
@@ -107,6 +113,7 @@ void loop()
       {
         Serial.println("0");
       }
+      Serial.println("IN TICK MODE");
     }
 
     //PID CONTROL
