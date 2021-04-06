03c45ac0fe642f42439b79d00bcb452f8bf3d18a
ajen8005 <ajj17b@my.fsu.edu>
2021-04-06 18:10:04 -0400

    d

--- Navigation/QRtest1.py
+++ Navigation/QRtest1.py
@@ -1,13 +1,30 @@
 import cv2
-import numpy as np
-import pyzbar.pyzbar as pyzbar
 
+# set up camera object
 cap = cv2.VideoCapture(0)
-font = cv2.FONT_HERSHEY_PLAIN
+
+# QR code detection object
+detector = cv2.QRCodeDetector()
+
 while True:
-    _, frame = cap.read()
-    decodedObjects = pyzbar.decode(frame)
-    for obj in decodedObjects:
-        print("Data", obj.data)
-        cv2.putText(frame, str(obj.data), (50, 50), font, 2,
-                    (255, 0, 0), 3)
+    # get the image
+    _, img = cap.read()
+    # get bounding box coords and data
+    data, bbox, _ = detector.detectAndDecode(img)
+    
+    # if there is a bounding box, draw one, along with the data
+    if(bbox is not None):
+        for i in range(len(bbox)):
+            cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i+1) % len(bbox)][0]), color=(255,
+                     0, 255), thickness=2)
+        cv2.putText(img, data, (int(bbox[0][0][0]), int(bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, (0, 255, 0), 2)
+        if data:
+            print("data found: ", data)
+    # display the image preview
+    cv2.imshow("code detector", img)
+    if(cv2.waitKey(1) == ord("q")):
+        break
+# free camera object and exit
+cap.release()
+cv2.destroyAllWindows()
