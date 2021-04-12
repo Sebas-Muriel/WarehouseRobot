b3cfcf162209bb40358122a1ce19433629d25b9a
ajen8005 <ajj17b@my.fsu.edu>
2021-04-12 03:15:32 -0400

    Fixed Train.py

--- UserInput/Train.py
+++ UserInput/Train.py
@@ -1,44 +1,103 @@
 #test
 # need these installed on py: sudo apt-get install python-serial
+#if i2c-1 not giving permissions run this command:
+#sudo chmod a+rw /dev/i2c-*
+#export DISPLAY=:0
+#Must plug in display first then turn on Pi to get camera to work
 import pymongo
 import sys
 import re
 import math
 import time
 import serial
+import json
 import cv2
+import RPi.GPIO as GPIO
+import adafruit_servokit
+import cv2 as cv
+import numpy as np
+from pyzbar.pyzbar import decode
 
 
-leftTick = bytearray()
-leftTick.append(0x03)
-leftNode = bytearray()
-leftNode.append(0x02)
-rightTick = bytearray()
-rightTick.append(0x05)
-rightNode = bytearray()
-rightNode.append(0x04)
-upTick = bytearray()
-upTick.append(0x07)
-upNode = bytearray()
-upNode.append(0x06)
-downTick = bytearray()
-downTick.append(0x09)
-downNode = bytearray()
-downNode.append(0x08)
+class ServoKit(object):
+    default_angle = 90
+
+    def __init__(self, num_ports):
+        print("Initializing the servo...")
+        self.kit = adafruit_servokit.ServoKit(channels=16)
+        self.num_ports = num_ports
+        self.resetAll()
+        print("Initializing complete.")
+
+    def setAngle(self, port, angle):
+        if angle < 0:
+            self.kit.servo[port].angle = 0
+        elif angle > 180:
+            self.kit.servo[port].angle = 180
+        else:
+            self.kit.servo[port].angle = angle
+    
+    def getAngle(self, port):
+        return self.kit.servo[port].angle
+
+    def reset(self, port):
+        self.kit.servo[port].angle = self.default_angle
+
+    def resetAll(self):
+        for i in range(self.num_ports):
+            self.kit.servo[i].angle = self.default_angle
+
+
 stopTick = bytearray()
-stopTick.append(0x01)
-stopNode = bytearray()
-stopNode.append(0x00)
-reset = bytearray()
-reset.append(0x80)
+stopTick.append(0x02)
+leftTick = bytearray()
+leftTick.append(0x06)
+rightTick = bytearray()
+rightTick.append(0x0A)
+upTick = bytearray()
+upTick.append(0x0E)
+downTick = bytearray()
+downTick.append(0x12)
 
+stopNode = bytearray()
+stopNode.append(0x01)
+leftNode = bytearray()
+leftNode.append(0x05)
+rightNode = bytearray()
+rightNode.append(0x09)
+upNode = bytearray()
+upNode.append(0x0D)
+downNode = bytearray()
+downNode.append(0x11)
+
+reset = bytearray()
+reset.append(0x00)
+
+stopMotor = bytearray()
+stopMotor.append(0x00)
+level1Motor = bytearray()
+level1Motor.append(0x20)
+level2Motor = bytearray()
+level2Motor.append(0x40)
+level3Motor = bytearray()
+level3Motor.append(0x80)
+pickupMotor = bytearray()
+pickupMotor.append(0x60)
+
+
+SRDY = 17 #yellow
+MRDY = 27 #orange
 
 ser = serial.Serial('/dev/ttyACM0',9600, timeout= 1)
+
+servoKit = ServoKit(4)
+# servoKit.setAngle(0, 110)
+# servoKit.setAngle(0, 145)
 firstFLG = 0
 START = ""
 startSerial = ""
 
-def main(package):
+def main(grid):
     myclient = MongoDBconnection()
     mydb = myclient["WarehouseMap"]
     mycol_packages = mydb["QRs"]
@@ -46,61 +105,116 @@ def main(package):
 
     mycol_nav.drop()
     mycol_packages.drop()
-    mycol_nav.insert_many(insertGrid(3,3))
+    gridDim = grid.split('x')
+    mycol_nav.insert_many(insertGrid(gridDim[1],gridDim[0]))
 
-    readQR = dict()
-    itemsList = list(dict())
     item = dict()
-    Dir = "Right"
+
 
     print("Beginning Training/Calibration\n\n")
 
+    # startSerial = ser.readline().decode('utf-8', 'ignore').rstrip()
+    START = "C3"
+    Dir = "Left"
 
-    startSerial = ser.readline().decode('utf-8', 'ignore').rstrip()
-    readQR = 0; #readQR returns the data
-    START = readQR["Name"]
-    mycol_nav.insert_one(readQR)
-    ser.write(rightTick)
-    count = 0
+    for collection in mycol_nav.find():
+        if collection["Name"] == START:
+            Nav = collection
+            break
+
+    ser.write(leftTick)
     while(1):
-        if readIRsensors == True:
-            count += 1
+        sensBool = readIRsensors()
+        if sensBool == 2:
+            #Move up to Read Pacakge
+            UART_send_repeat(upTick)
+            while(1):
+                if readIRsensors() == 1:
+                    break
+            #Read QR code for both heights
             for i in range(0,2):
-                if (Dir == "Right"):
-                    item["Left"] = readQR["Name"]
-                    item["Right"] = readQR["Right"]
+                if i == 0:
+                    servoKit.setAngle(0, 110)
                 else:
-                    item["Left"] = readQR["Left"]
-                    item["Right"] = readQR["Name"]
-                item["Level"] = i
-                item["Item"] = "Test1" #read from QR
-                #Append item JSON to the whole list
-                mycol_packages.insert_one(item)
-                #itemsList.append(item)#
-                #move motor up to the next level
-        if count == 4:
-            readQR = 0 #read the node qr
+                    servoKit.setAngle(0, 145)
+                QR = readQR()
+                if QR != "":
+                    QR = json.loads(QR)
 
-            if (endOfTraining(readQR, Dir) == True):
+                    if (Dir == "Right"):
+                        item["Left"] = Nav["Name"]
+                        item["Right"] = Nav["Right"]
+                    else:
+                        item["Left"] = Nav["Left"]
+                        item["Right"] = Nav["Name"]
+                    item["Level"] = i+1
+                    item["Item"] = QR["Item"] 
+                    mycol_packages.insert_one(item)
+            #Go back on main line
+            UART_send_repeat(downNode)
+            while readIRsensors != 1:
+                pass
+            #Continue going the direction
+            if Dir == "Right":
+                UART_send_repeat(rightTick)
+            else:
+                UART_send_repeat(leftTick)
+
+        elif sensBool == 1:
+            #Get node in the direction that it is moving
+            if Dir == "Right":
+                tempNav = Nav["Right"]
+                for collection in mycol_packages.find():
+                    if collection["Name"] == tempNav:
+                        Nav = collection
+                        break
+            else:
+                tempNav = Nav["Left"]
+                for collection in mycol_packages.find():
+                    if collection["Name"] == tempNav:
+                        Nav = collection
+                        break
+            
+            if (endOfTraining(Nav, Dir) == True):
                 print('End of Training going back to start')
-                returnToStart(START, readQR["Name"])
-            elif (moveUpLogic(readQR, Dir) == True):
-                ser.write(upNode)
-                while(readIRsensors() == False):
-                    pass
-                #Swamp direction
+                nodeMove(convertToGrid(Nav["Item"]), convertToGrid(START))
+                return
+            elif (moveUpLogic(Nav, Dir) == True):
+                UART_send_repeat(downNode)
+                while(readIRsensors() != 1):
+                        pass
+                #Swap direction
                 if (Dir == "Left"):
                     Dir = "Right"
-                    ser.write(rightTick)
+                    UART_send_repeat(rightTick)
                 else:
                     Dir = "Left"
-                    ser.write(leftTick)
-                readQR = 0 #readQR
-                mycol_nav.insert_one(readQR)
+                    UART_send_repeat(leftTick)
             else:
-                mycol_nav.insert_one(readQR)
-            count = 0
-                
+                if Dir == "Left":
+                    UART_send_repeat(leftTick)
+                else:
+                    UART_send_repeat(rightTick)
+
+def PinSetup():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(MRDY, GPIO.OUT)
+    GPIO.setup(SRDY, GPIO.IN)
+    GPIO.output(MRDY, GPIO.HIGH)
+
+def readIRsensors():
+    read_serial = ser.readline().decode('utf-8', 'ignore').rstrip()
+    print(read_serial)
+    if read_serial == "1":
+        return 1
+    elif read_serial == "2":
+        return 2
+    elif read_serial == "3":
+        return 3
+    else:
+        return 0
+
 
    
 
@@ -115,6 +229,33 @@ def MongoDBconnection():
     mydb = myclient["WarehouseMap"]
     print(mydb.list_collection_names())
     return myclient
+
+def readQR():
+    # set up camera object
+    cap = cv.VideoCapture(0)
+    # cap.set(3,640)
+    # cap.set(4,480)
+    myData = ""
+    startTime = time.time()
+    while True:
+        ret, frame = cap.read()
+        currentTime = time.time()
+
+        for barcode in decode(frame):
+            #print(barcode.data)
+            myData = barcode.data.decode('utf-8')
+            print(myData)
+            pts = np.array([barcode.polygon],np.int32)
+            cv.polylines(frame,[pts],True,(255,0,0),5)
+            pts2 = barcode.rect
+            cv.putText(frame,myData,(pts2[0],pts2[1]), cv.FONT_HERSHEY_COMPLEX,1,(255,0,0),2)
+
+        cv.imshow('In',frame)
+        if currentTime - startTime >= 3:
+            break
+        if myData != "":
+            return myData
+    return myData
 
 def insertGrid(numCol, numRow):
     mongoInput = list(dict())
@@ -146,27 +287,14 @@ def insertGrid(numCol, numRow):
             mongoInput.append(dict(item))
     return mongoInput
 
-def readIRsensors():
-    read_serial = ser.readline().decode('utf-8', 'ignore').rstrip()
-    print(read_serial)
-    if read_serial == "1":
-        return True
-    else:
-        return False
-
 def endOfTraining(readQR2, Dir):
     if (Dir == "Left"):
-        if (readQR2["Left"] == "null" and readQR2["Up"] == "null"):
+        if (readQR2["Left"] == "null" and readQR2["Down"] == "null"):
             return True
     else:
-        if (readQR2["Right"] == "null" and readQR2["Up"] == "null"):
+        if (readQR2["Right"] == "null" and readQR2["Down"] == "null"):
             return True
     return False
-
-def returnToStart(start, end):
-    st = convertToGrid(start)
-    ed = convertToGrid(end)
-    return
 
 def moveUpLogic(readQR2, Dir):
     if (Dir == "Left"):
@@ -194,6 +322,75 @@ def convertToGrid(node):
     split[0] = ord(temp[0]) - 65
     split[1] = int(split[1])
     return split
+
+def UART_send(message):
+    GPIO.output(MRDY, GPIO.LOW)
+    while(GPIO.input(SRDY) == GPIO.HIGH):
+        pass
+    ser.write(message)
+    GPIO.output(MRDY, GPIO.HIGH)
+    while(GPIO.input(SRDY) == GPIO.LOW):
+        pass
+    return
+
+def UART_send_repeat(message):
+    count = 0
+    startTime = time.time()
+    
+    while(1):
+        currentTime = time.time()
+        if (currentTime - startTime > .001):
+            count = count + 1
+            UART_send(message)
+            startTime = time.time()
+        if count == 4:
+            break
+    return
+
+def nodeMove(start, end):
+    count = 0
+
+    if start[0] == end[0]:
+        if start[1] != end[1]:
+            if (start[1] > end[1]):
+                UART_send_repeat(downNode)
+            else:
+                UART_send_repeat(upNode)
+    else:
+        if (start[0] > end[0]):
+            UART_send_repeat(leftNode)
+        else:
+            UART_send_repeat(rightNode)
+    Horizontal = True
+    while(1):
+        print(start)
+        if start[0] == end[0]:
+            Horizontal = False
+            if start[1] == end[1]:
+                break
+        if Horizontal == True:
+            #Go Horizontal first
+            if (start[0] > end[0]):
+                navMove("Left", start)
+            else:
+                navMove("Right", start)
+        else:
+            #Go Vertically  
+            if (start[1] > end[1]):
+                navMove("Down", start)
+            else:
+                navMove("Up", start)
+
+        if start[0] == end[0]:
+            if count == 0:
+                if (start[1] > end[1]):
+                    UART_send_repeat(downNode)
+                else:
+                    UART_send_repeat(upNode)
+            count = count + 1
+            Horizontal = False
+    UART_send_repeat(stopTick)
+    return
 
 if __name__ == "__main__":
     main(sys.argv[1])
